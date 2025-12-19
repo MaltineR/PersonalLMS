b73f8ecca728eeae54d8c251b44ba48a0367d422
MaltineR <maltina.rama@gmail.com>
2025-12-19 19:32:57 +0100

    Vite API url

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-//<reference types="vitest" />
+
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
